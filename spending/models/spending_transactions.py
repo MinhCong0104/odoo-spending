338f06053fe2d917efdcb84b0e77902515152d57
minhcong <tmcong1401@gmail.com>
2024-08-17 16:25:00 +0700

    Update transactions

--- spending/models/spending_transactions.py
+++ spending/models/spending_transactions.py
@@ -5,7 +5,7 @@ import logging
 from odoo import api, fields, models, _
 
 
-TYPES_OF_TRANSACTION = [('spend', 'Spend'), ('income', 'Income'), ('save', 'Save')]
+TYPES_OF_TRANSACTION = [('spend', 'Spend'), ('income', 'Income'), ('save', 'Save'), ('internal', 'Internal')]
 
 
 _logger = logging.getLogger(__name__)
@@ -15,34 +15,22 @@ class Transactions(models.Model):
     _name = 'spending.transactions'
     _description = 'Spending Transactions'
 
-    @api.model
-    def default_get(self, fields):
-        result = super(Transactions, self).default_get(fields)
-        if 'recipient_ids' in fields and result.get('mail_message_id'):
-            mail_message_id = self.env['mail.message'].browse(result['mail_message_id'])
-            result['recipient_ids'] = [(0, 0, {
-                'notification_id': notif.id,
-                'resend': True,
-                'failure_type': notif.failure_type,
-                'partner_name': notif.res_partner_id.display_name or mail_message_id.record_name,
-                'sms_number': notif.sms_number,
-            }) for notif in mail_message_id.notification_ids if notif.notification_type == 'sms' and notif.notification_status in ('exception', 'bounce')]
-        return result
-
     date = fields.Date(default=fields.Date.today(), required=True)
     amount = fields.Monetary(required=True, currency_field='currency_id')
     currency_id = fields.Many2one("res.currency", string='Currency', required=True)
-    type = fields.Selection(TYPES_OF_TRANSACTION, required=True, default='spend', compute='_compute_type', store=True)
+    type = fields.Selection(TYPES_OF_TRANSACTION,  default='spend',
+                            compute='_compute_type', store=True, required=True, readonly=False)
     purpose = fields.Selection([('transaction', 'Transaction'), ('save', 'Save'), ('invest', 'Invest')])
     category_id = fields.Many2one('spending.categories')
     is_save = fields.Boolean()
     from_account = fields.Many2one('spending.accounts')
     to_account = fields.Many2one('spending.accounts')
-    note = fields.Char()
+    note = fields.Text()
     user_id = fields.Many2one('res.users', readonly=True)
 
-    @api.depends("category_id")
-    def _compute_type(self):
-        for rec in self:
-            if rec.category_id:
-                rec.type = rec.category_id.type
+    # Tính toán trường purpose (mục đích sử dụng) dựa trên đặc điểm của giao dịch
+    # 1. chi tiêu: type = spend
+    # 2. thu nhập: type = income
+    # 3. chuyển tiền nội bộ: type = internal, cả tài khoản gửi và nhận đều là tài khoản chi tiêu
+    # 4. tiết kiệm: type = internal, tài khoản nhận là save
+    # 5. đầu tư: type = internal, tài khoản nhận là invest
