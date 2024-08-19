24d4ec2e3957e5bd085aa8474f9d962b22d6765a
Minh Cong <congtm.bhsoft@gmail.com>
2024-08-19 17:44:09 +0700

    [IMP] Thinh Cuong Accounting

---
+++ tc_accounting/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Thinh Cuong Accounting',
+    'description': """Allow users manage their spending""",
+    'version': '1.0',
+    'category': 'Accounting/Accounting',
+    'sequence': -100,
+    'depends': ['account'],
+    'data': [
+        "security/ir.model.access.csv",
+        "views/account_areas_views.xml",
+        "views/menus.xml",
+    ],
+    # 'assets': {
+    #     'web.assets_frontend': [
+    #         'auth_signup/static/**/*',
+    #     ],
+    # },
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ tc_accounting/models/acount_areas.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+_logger = logging.getLogger(__name__)
+
+
+class Limit(models.Model):
+    _name = 'account.areas'
+    _description = 'Account Areas'
+
+    name = fields.Char(required=True)
+    code = fields.Char(required=True)
+    company_id = fields.Many2one('res.companies', required=True)
+    note = fields.Char()
+
+    @api.constrains('name', 'code')
+    def _check_name_code(self):
+        a = self.search_read([], fields=['name', 'code'])
+        existing_names = []
+        existing_codes = []
+        for rec in self:
+            if rec.name in existing_names:
+                raise ValidationError(_('You are not allowed to request a time off on a Mandatory Day.'))
+            if rec.code in existing_codes:
+                raise ValidationError(_('You are not allowed to request a time off on a Mandatory Day.'))
